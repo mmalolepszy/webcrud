46a34a09d0e66c521f6b51a394a52740f915582e
Michal Malolepszy <michal.malolepszy@altkom.pl>
2016-03-08 13:01:14 +0100

    Fixed problems in production environment

--- src/WebCRUD.vNext/gulpfile.js
+++ src/WebCRUD.vNext/gulpfile.js
@@ -14,6 +14,8 @@ var paths = {
 
 paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.pluginExtensions = paths.webroot + "js/Application/plugins.extensions.js";
+paths.pluginExtensionsDest = paths.webroot + "js/Application/plugins.extensions.min.js";
 paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
@@ -30,8 +32,15 @@ gulp.task("clean:css", function (cb) {
 gulp.task("clean", ["clean:js", "clean:css"]);
 
 gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+    return gulp.src([paths.js, "!" + paths.minJs, "!" + paths.pluginExtensions], { base: "." })
         .pipe(concat(paths.concatJsDest))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:js:pluginExtensions", function () {
+    return gulp.src([paths.pluginExtensions], { base: "." })
+        .pipe(concat(paths.pluginExtensionsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
 });
@@ -43,7 +52,7 @@ gulp.task("min:css", function () {
         .pipe(gulp.dest("."));
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task("min", ["min:js", "min:css", "min:js:pluginExtensions"]);
 
 //replace internal with public in resource file on project build
 //to make resources working on view model classes
