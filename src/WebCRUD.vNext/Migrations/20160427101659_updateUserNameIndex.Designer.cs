5d6dd61dc5a12a7725282e2cae2600935e8e74fb
Michał Małolepszy <michal.malolepszy@altkom.pl>
2016-04-27 12:18:32 +0200

    Added migration to address latest changes in AspNetCore.Identity db structure

--- src/WebCRUD.vNext/Migrations/20160427101659_updateUserNameIndex.Designer.cs
+++ src/WebCRUD.vNext/Migrations/20160427101659_updateUserNameIndex.Designer.cs
@@ -8,9 +8,10 @@ using WebCRUD.vNext.Models;
 namespace WebCRUD.vNext.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160427101659_updateUserNameIndex")]
+    partial class updateUserNameIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rc3-20703");
---
+++ src/WebCRUD.vNext/Migrations/20160427101659_updateUserNameIndex.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebCRUD.vNext.Migrations
+{
+    public partial class updateUserNameIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName");
+        }
+    }
+}
